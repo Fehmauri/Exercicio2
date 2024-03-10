d3c22ea7c1d724d10bc4a90fb2d71eb47685d8c2
Felipe Mauri <felipe-mauri@hotmail.com>
2024-03-10 18:20:49 -0300

    Finalizado o Exercicio2

---
+++ Exercicio2/Enquete/EnqueteCalculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enquete
+{
+    public class EnqueteCalculo
+    {
+        public static readonly string[] sistemas = { "Windows Server", "Unix", "Linux", "Netware", "Mac OS", "Outro" };
+        public static readonly int[] votos = new int[sistemas.Length];
+        public static readonly double[] porcentagens = new double[sistemas.Length];
+
+
+        public static void CalculoDasPorcentagens()
+        {
+            int totalVotos = votos.Sum();
+            for (int i = 0; i < votos.Length; i++)
+            {
+                double porcentagem = (double)votos[i] / totalVotos * 100;
+                porcentagens[i] = Math.Round(porcentagem, 1);
+            }
+        }
+
+        public static void ApresentacaoDosResultados()
+        {
+            Console.WriteLine("Sistema Operacional Votos   %");
+            Console.WriteLine("------------------- ----- -----");
+            for (int i = 0; i < sistemas.Length; i++)
+            {
+                Console.WriteLine("{0,-18} {1,5} {2,6:F1}%", sistemas[i], votos[i], porcentagens[i]);
+            }
+            Console.WriteLine("------------------- ----- -----");
+
+            int vencedor = Array.IndexOf(votos, votos.Max());
+            Console.WriteLine("O Sistema Operacional mais votado foi o {0}, com {1} votos, correspondendo a {2}% dos votos.",
+            sistemas[vencedor], votos[vencedor], porcentagens[vencedor]);
+        }
+
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Enquete;
+
+namespace Exercicio2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int voto;
+            do
+            {
+                Console.Clear();
+                string mensagem = String.Format(
+@"Qual o melhor Sistema Operacional para uso em servidores?
+
+As possíveis respostas são:
+
+1- Windows Server
+2- Unix
+3- Linux
+4- Netware
+5- Mac OS
+6- Outro
+
+Digite o número do sistema operacional (1-6) ou 0 para sair: ");
+
+                Console.WriteLine(mensagem);
+                voto = int.Parse(Console.ReadLine());
+
+                if (voto < 0 || voto > 6)
+                {
+                    Console.WriteLine("Voto inválido! Digite um número entre 1 e 6 ou 0 para sair.");
+                    voto = int.Parse(Console.ReadLine());
+                }
+                if (voto > 0)
+                {
+                    EnqueteCalculo.votos[voto - 1]++;
+                }
+
+
+            } while (voto != 0);
+            Console.Clear();
+            EnqueteCalculo.CalculoDasPorcentagens();
+            EnqueteCalculo.ApresentacaoDosResultados();
+        }
+    }
+}
